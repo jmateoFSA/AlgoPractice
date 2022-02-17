fe4bbe72966a50ec49e52df1ebc69b14fe4ede87
Juan Mateo <throwaway4classjmateo@gmail.com>
2022-02-17 17:59:56 -0500

    update: more palindrome tests and removed console log

--- Palindrome/palindrome.spec.js
+++ Palindrome/palindrome.spec.js
@@ -1,10 +1,10 @@
 const { expect } = require('chai')
 const isPalindrome = require('./palindrome')
 
-console.log(isPalindrome(1215))
-
 describe('isPalindrome', () => {
   it("should return true if the integer when reversed is the same or false if not", () => {
-    expect(isPalindrome(121)).to.be.true
+    expect(isPalindrome(121)).to.be.true;
+    expect(isPalindrome(-121)).to.be.false;
+    expect(isPalindrome(10)).to.be.false;
   })
 })
